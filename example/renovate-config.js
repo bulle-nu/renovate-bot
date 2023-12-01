29eb5ae7c166ef3ed88b4aba8a267b799eda32b6
Jocke <jocke@bulle.nu>
2023-12-01 22:08:08 +0100

    updated config

---
+++ example/renovate-config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  branchPrefix: "test-renovate/",
+  username: "renovate-release",
+  gitAuthor: "Renovate Bot <bot@renovateapp.com>",
+  onboarding: false,
+  platform: "github",
+  includeForks: true,
+  dryRun: "full",
+  repositories: ["ch0nen/8_jobs_and_docker"],
+  packageRules: [
+    {
+      description: "lockFileMaintenance",
+      matchUpdateTypes: [
+        "pin",
+        "digest",
+        "patch",
+        "minor",
+        "major",
+        "lockFileMaintenance",
+      ],
+      dependencyDashboardApproval: false,
+      stabilityDays: 0,
+    },
+  ],
+};
