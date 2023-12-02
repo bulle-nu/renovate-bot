a9d92b1683e977fc85730e0d1edc258e815ef439
Jocke <jocke@bulle.nu>
2023-12-02 12:20:23 +0100

    updated repos

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ module.exports = {
   platform: "github",
   includeForks: true,
   dryRun: "full",
-  repositories: ["ch0nen/8_jobs_and_docker"],
+  repositories: ["bulle-nu/8_jobs_and_docker.git"],
   packageRules: [
     {
       description: "lockFileMaintenance",
